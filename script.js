8f5b927211b9c2104988f5f9a6aa0895d7ff1029
KikiCheung <70065193+KikiCheung@users.noreply.github.com>
2020-09-15 14:29:26 +0800

    add css and basic js

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
 //Selectors
+const todoInput = document.querySelector(".to-do-input");
+const todoButton = document.querySelector(".to-do-button");
+const todoList = document.querySelector(".to-do-list");
 
 //Event Listeners
+todoButton.addEventListener("click", addToDo);
 
 //Functions
+function addToDo(event) {
+  event.preventDefault();
+  //Create todo Div
+  const toDoDiv = document.createElement("div");
+  toDoDiv.classList.add("todo");
+  //Create todo LI
+  const newToDo = document.createElement("li");
+  newToDo.innerText = " hey";
+  newToDo.classList.add("to-do-item");
+  toDoDiv.appendChild(newToDo);
+  //Checked mark button
+  const completedButton = document.createElement("button");
+  completedButton.innerHTML = '<i class="fas fa-check"></i>';
+  completedButton.classList.add("complete-btn");
+  toDoDiv.appendChild(completedButton);
+  //Checked trash button
+  const trashButton = document.createElement("button");
+  trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+  trashButton.classList.add("trash-btn");
+  toDoDiv.appendChild(trashButton);
+  //Append tolist
+  todoList.appendChild(toDoDiv);
+}
